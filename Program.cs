77ab08af53832db3e80d53a52c824445aada6ebf
hossam01101998 <hossam1998hossam@gmail.com>
2023-09-26 14:49:38 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp6;
+using Diversen;
+
+Console.WriteLine("Hello, World!");
+
+Student student = new Student { Voornaam = "Jamil", ID = 1 , Familienaam = "NARO", Inschrijving = DateTime.Now};
+
+
+string voornaam, achternaam;
+
+Tuple<string, string> NaamVanStudent = Student.Naam(1);
+
+voornaam = NaamVanStudent.Item1;
+achternaam = NaamVanStudent.Item2;
+
+
+
+
---
+++ Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Student
+    {
+        public int ID { get; set; }
+        public string Voornaam { get; set; }
+        public string Familienaam { get; set; }
+
+        public DateTime Inschrijving { get; set; }
+        static List<Student> studenten = new List<Student>();
+
+        public Student() {
+            studenten.Add(this);
+
+        }
+
+        public Tuple<string, string> Naam(int ID)
+        {
+            Student student = null;
+            foreach (Student s in studenten)
+                if (s.ID == ID)
+                    student = s;
+            return Tuple.Create (student.Voornaam, student.Familienaam);
+        }
+
+        public override string ToString()
+        {
+            return "Id = " + ID + base.ToString;
+        }
+        
+
+        public static StudentenNaam StructNaam (int id)
+        {
+            Student student = null;
+            foreach (Student s in studenten)
+                if (s.ID == id)
+                    student = s;
+            StudentenNaam sn = new StudentenNaam();
+            return new StudentenNaam { Voornaam = student.Voornaam, Familienaam = student.Familienaam };
+        }
+        public struct StudentenNaam
+        {
+            public string Voornaam { get; set; }
+            public string Familienaam { get; set; }
+            public override string ToString()
+            {
+                return "Naam = " + Voornaam + " " + Familienaam;
+
+            }
+        }
+
+        
+
+    }
+}
